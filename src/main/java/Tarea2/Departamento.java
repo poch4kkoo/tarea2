0dfea0605de1ea41267789750eaf8afa004775d6
pbascunan2025 <pbascunan2025@inf.udec.cl>
2026-05-17 16:56:05 -0400

    Avance en clases Nota y Departamento

--- src/main/java/Tarea2/Departamento.java
+++ src/main/java/Tarea2/Departamento.java
@@ -1,4 +1,41 @@
 package Tarea2;
 
-public class Departamento {
+import java.util.List;
+
+public class Departamento implements Invitable {
+    private String nombre;
+    private List<Empleado> empleados;
+
+
+    public Departamento(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int obtenerCantidadEmpleados(){
+        return this.empleados.size();
+    }
+
+    @Override
+    public void invitar () {
+        System.out.println("Invitar departamento completo: " + this.nombre);
+        for (int i = 0; i < empleados.size(); i++) {
+            Empleado emp = empleados.get(i);
+            emp.invitar();
+        }
+    }
+
+    //Getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
 }
--- src/main/java/Tarea2/Nota.java
+++ src/main/java/Tarea2/Nota.java
@@ -2,4 +2,23 @@ package Tarea2;
 
 public class Nota {
     private String contenido;
+
+    public Nota(String contenido) {
+        this.contenido = contenido;
+    }
+
+    //Getters, setters y toString
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    @Override
+    public String toString() {
+        return contenido;
+    }
 }
