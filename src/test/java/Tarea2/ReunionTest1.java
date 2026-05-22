eade2f03020e2b10f647e9e51bfb372f4c166515
pbascunan2025 <pbascunan2025@inf.udec.cl>
2026-05-22 11:55:31 -0400

    Mas Tests

--- src/test/java/Tarea2/ReunionTest1.java
+++ src/test/java/Tarea2/ReunionTest1.java
@@ -14,29 +14,37 @@ class ReunionTest1 {
 
     // Variables globales para el escenario de prueba
     private Reunion reunion;
-    private Empleado empNormal;
-    private Empleado empAtrasado;
-    private Empleado empAusente;
-    Instant horaInicio = Instant.now();
+    private Empleado emp1;
+    private Empleado emp2;
+    private Empleado emp3;
+
+    // Tiempos para las pruebas
+    private Instant horaInicio = Instant.now();
+    private Instant horaFin = horaInicio.plus(Duration.ofMinutes(45));
+    private Instant minAtraso = horaInicio.plus(Duration.ofMinutes(10));
 
     // Antes de ejecutar cada test, le presentamos estas condiciones iniciales.
     @BeforeEach
     void setUp() {
         // Inicializamos una reunión virtual
-        reunion = new ReunionVirtual(tipoReunion.TECNICA, new Date(), horaInicio, Duration.ofMinutes(60), horaInicio, horaInicio.plus(Duration.ofMinutes(45)), "www.enlace.com");
+        reunion = new ReunionVirtual(tipoReunion.TECNICA, new Date(), horaInicio, Duration.ofMinutes(60), horaInicio, horaFin, "www.enlace.com");
 
         // Creamos 3 empleados
-        empNormal = new Empleado("123", "Sinson", "Homero", "hs@udec.cl");
-        empAtrasado = new Empleado("456", "Sinson", "Bart", "bs@udec.cl");
-        empAusente = new Empleado("789", "Sinson", "Lisa", "ls@udec.cl");
+        emp1 = new Empleado("123", "Sinson", "Homero", "hs@udec.cl");
+        emp2 = new Empleado("456", "Sinson", "Bart", "bs@udec.cl");
+        emp3 = new Empleado("789", "Sinson", "Lisa", "ls@udec.cl");
+
+        reunion.getInvitaciones().add(new Invitacion(emp1, horaInicio));
+        reunion.getInvitaciones().add(new Invitacion(emp2, horaInicio));
+        reunion.getInvitaciones().add(new Invitacion(emp3, horaInicio));
 
     }
 
     @Test
-    @DisplayName("Test de asistencias")
+    @DisplayName("Test para verificar lista de asistencias")
     void obtenerAsistencias() {
-        Asistencia asistenciaHomero = new Asistencia(empNormal);
-        Retraso retrasoBart = new Retraso(empAtrasado, Instant.now());
+        Asistencia asistenciaHomero = new Asistencia(emp1);
+        Retraso retrasoBart = new Retraso(emp2, minAtraso);
 
         reunion.getAsistencias().add(asistenciaHomero);
         reunion.getAsistencias().add(retrasoBart);
@@ -44,17 +52,16 @@ class ReunionTest1 {
         List<Asistencia> listaAsistencias = reunion.obtenerAsistencias();
 
         // Comprobamos que ambos están guardados en la lista de asistencia
-        assertNotNull(listaAsistencias, "La lista de asistencias no debería ser nula");
         assertEquals(2, listaAsistencias.size(), "Deberían haber exactamente 2 registros en asistencias");
-        assertTrue(listaAsistencias.contains(asistenciaHomero), "Debería incluir la asistencia de Homero");
-        assertTrue(listaAsistencias.contains(retrasoBart), "Debería incluir la asistencia de Bart");
+        assertTrue(listaAsistencias.contains(asistenciaHomero), "asistencia de Homero");
+        assertTrue(listaAsistencias.contains(retrasoBart), "asistencia de Bart");
     }
 
     @Test
-    @DisplayName("Test de retrasos")
+    @DisplayName("Test para verificar lista de retrasos")
     void obtenerRetrasos() {
-        Asistencia asistenciaHomero = new Asistencia(empNormal);
-        Retraso retrasoBart = new Retraso(empAtrasado, Instant.now()); // Subclase Retraso
+        Asistencia asistenciaHomero = new Asistencia(emp1);
+        Retraso retrasoBart = new Retraso(emp2, horaInicio.plus(Duration.ofMinutes(10))); // Subclase Retraso
 
         reunion.getAsistencias().add(asistenciaHomero);
         reunion.getAsistencias().add(retrasoBart);
@@ -63,14 +70,27 @@ class ReunionTest1 {
         List<Asistencia> listaRetrasos = reunion.obtenerRetrasos();
 
         assertEquals(1, listaRetrasos.size(), "Debería haber 1 persona en la lista de retrasos");
-        assertEquals(empAtrasado, listaRetrasos.getFirst().getEmpleado(), "El empleado atrasado deberia ser Bart");
+        assertEquals(emp2, listaRetrasos.getFirst().getEmpleado(), "El empleado atrasado deberia ser Bart");
     }
 
     @Test
     void obtenerTotalAsistencia() {
+
+        reunion.getAsistencias().add(new Asistencia(emp1));
+        reunion.getAsistencias().add(new Asistencia(emp2));
+
+        assertEquals(2, reunion.obtenerTotalAsistencia(), "Debe ser 2");
+
     }
 
     @Test
     void obtenerPorcentajeAsistencia() {
+
+        reunion.getAsistencias().add(new Asistencia(emp1));
+        reunion.getAsistencias().add(new Retraso(emp2, minAtraso));
+
+
+        float porcentaje = reunion.obtenerPorcentajeAsistencia();
+        assertEquals(66.67f, porcentaje, 0.01f, "Asisten 2 de los 3 invitados, deberia dar 66.67%");
     }
 }
