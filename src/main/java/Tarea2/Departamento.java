f1235f7b320f017dab26692e9130a705bb583f9a
pbascunan2025 <pbascunan2025@inf.udec.cl>
2026-05-23 02:07:46 -0400

    Se completo DepartamentoTest y se documento la clase Departamento

--- src/main/java/Tarea2/Departamento.java
+++ src/main/java/Tarea2/Departamento.java
@@ -3,24 +3,48 @@ package Tarea2;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Representa un Departamento el cual puede contener grandes numeros
+ * de empleados, lo que permite separrlos en grupos especificos
+ * y gestionar muchas invitaciones rapidamente.
+ */
 public class Departamento implements Invitable {
     private String nombre;
     private List<Empleado> empleados;
 
 
+    /**
+     * Construye un Departamento y la lista de empleados
+     * @param nombre El nombre del departamento.
+     */
     public Departamento(String nombre) {
         this.nombre = nombre;
         this.empleados = new ArrayList<>();
     }
 
+    /**
+     * Añade un nuevo empleado a este departamento.
+     *
+     * @param empleado El Empleado que se integrará al departamento.
+     */
     public void agregarEmpleado(Empleado empleado) {
         this.empleados.add(empleado);
     }
 
+    /**
+     * Calcula la cantidad de empleados del departamento
+     *
+     * @return Cantidad de empleados como entero
+     */
     public int obtenerCantidadEmpleados(){
         return this.empleados.size();
     }
 
+
+    /**
+     * Se encarga de invitar a todo el departamento uno por uno
+     */
     @Override
     public void invitar () {
         System.out.println("Invitar departamento completo: " + this.nombre);
--- src/test/java/Tarea2/DepartamentoTest.java
+++ src/test/java/Tarea2/DepartamentoTest.java
@@ -3,6 +3,10 @@ package Tarea2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -38,4 +42,41 @@ public class DepartamentoTest {
         assertEquals(0, totalInicial, "Un departamento con 0 empleados");
 
     }
+
+    @Test
+    @DisplayName("Test de invitar Departamento completo a la Reunion")
+
+    public void ReunionDepartamentoTest() {
+
+        // Se crea el departamento
+        Departamento depto = new Departamento("deptoTest");
+        Empleado emp1 = new Empleado("123", "Sinson", "Homero", "hs@udec.cl");
+        Empleado emp2 = new Empleado("456", "Sinson", "Bart", "bs@udec.cl");
+
+        depto.agregarEmpleado(emp1);
+        depto.agregarEmpleado(emp2);
+
+
+        // Se crea la reunion
+        Empleado organizador = new Empleado("Test", "Test", "Test", "Test");
+        Reunion reunion = new ReunionPresencial(tipoReunion.DIRECTIVA,
+                new Date(), Instant.now(), Duration.ofMinutes(60),
+                organizador, "Sala 2");
+
+
+        // Se invita al departamento
+        depto.invitar();
+
+        //Se registra la asistencia
+        for (Empleado emp : depto.getEmpleados()) {
+            reunion.getInvitaciones().add(new Invitacion(emp, Instant.now()));
+            reunion.getAsistencias().add(new Asistencia(emp));
+        }
+
+        // Validaciones
+        assertEquals(2, reunion.obtenerTotalAsistencia(), "El total de asistentes debe ser 2");
+        assertEquals(100.0f, reunion.obtenerPorcentajeAsistencia(), "La asistencia debería ser del 100%");
+    }
+
+
 }
